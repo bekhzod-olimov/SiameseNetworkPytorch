cb3ecbd562d7aa7cb476550481f4f0b05bde8123
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-01-02 11:16:36 +0900

    Update model.py

--- model.py
+++ model.py
@@ -5,6 +5,8 @@ class Model(torch.nn.Module):
     # Initialize model with model name and embedding size
     def __init__(self, model_name, emb_size=512):
         super(Model, self).__init__()
+        
+        # Get the base model
         self.eff = timm.create_model(model_name, pretrained=True)
         self.eff.classifier = torch.nn.Linear(in_features=self.eff.classifier.in_features, out_features=emb_size)
         print(f"\nModel {model_name} is successfully loaded!")
