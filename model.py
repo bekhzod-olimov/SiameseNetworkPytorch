d8fafc8a50badc999386671abe983ee75664bb0e
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-03-29 16:07:02 +0900

    Update model.py

--- model.py
+++ model.py
@@ -2,8 +2,19 @@ import torch, timm
 
 class Model(torch.nn.Module):
     
+    """
+    
+    This class gets a model name and feature embedding size and returns a model to be trained.
+    
+    Arguments:
+    
+        model_name   - a name of the model in timm librariy to be trained, str;
+        emb_size     - feature embedding dimension, int.
+    
+    """
+    
     # Initialize model with model name and embedding size
-    def __init__(self, model_name, emb_size=512):
+    def __init__(self, model_name, emb_size = 512):
         super(Model, self).__init__()
         
         # Get the base model
