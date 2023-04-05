210c352b22fc615974256b9a20edcd5830790ace
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-04-05 16:49:03 +0900

    model script

--- model.py
+++ model.py
@@ -22,10 +22,10 @@ class Model(torch.nn.Module):
         super(Model, self).__init__()
         
         # Get the base model
-        self.eff = timm.create_model(model_name, pretrained=True)
+        self.eff = timm.create_model(model_name, pretrained = True)
         
         # Change the classifier part of the base model
-        self.eff.classifier = torch.nn.Linear(in_features=self.eff.classifier.in_features, out_features=emb_size)
+        self.eff.classifier = torch.nn.Linear(in_features = self.eff.classifier.in_features, out_features = emb_size)
         print(f"\nModel {model_name} is successfully loaded!")
         
     # Get feature maps and return them as an output
